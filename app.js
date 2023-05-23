6cf08cbb28d7c9bcf48de3e4782313e0ea7dea97
ubaid567 <124505833+ubaid567@users.noreply.github.com>
2023-05-22 19:31:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+//java script assignment 7
+//chapter 26-30
+//question # 1
+//  var integers=prompt("enter a integers")
+//  var roundOff= Math.round(integers)
+//  var floor= Math.floor(integers)
+//  var ceil= Math.ceil(integers)
+
+//  if(integers.charCodeAt(0) !="45" ){
+//     console.log("integers = "+integers)
+//     console.log("roundoff = "+roundOff)
+//     console.log("floor = "+floor)
+//     console.log("ceil = "+ceil)
+//  }
+
+
+//question # 2
+//  var integers=prompt("enter a negative  integers")
+//  var roundOff= Math.round(integers)
+//  var floor= Math.floor(integers)
+//  var ceil= Math.ceil(integers)
+
+//  if(integers.charCodeAt(0) =="45" ){
+//     console.log("integers = "+integers)
+//     console.log("roundoff = "+roundOff)
+//     console.log("floor = "+floor)
+//     console.log("ceil = "+ceil)
+//  }else{
+//     console.log("input is not a nagative integer")
+//  }
+
+
+//question # 3
+// var input=prompt("input a number")
+// if(input.charCodeAt(0)==45){
+//     console.log("the absolute value of "+input+" = "+input.slice(1) )
+// }else{
+//     console.log("the absolute value of "+input+" = "+input)
+// }
+
+// question # 4
+// var diceValue =(Math.random()*6)+1
+// console.log("The random dice value = "+ Math.floor(diceValue))
+
+
+// question # 5
+// var diceValue =(Math.random()*2)+1
+// if(Math.floor(diceValue)==2){
+//     console.log("2")
+//     console.log('Random coin value is "Heads" ')
+// }else{
+//     console.log("1")
+//     console.log('Random coin value is "Tails" ')
+// }
+
+
+//question # 6
+// var diceValue =(Math.random()*100)+1
+// console.log("The random between 1 to 100 is= "+ Math.floor(diceValue))
+
+
+// question # 7
+// var whight =prompt("enter your wheight")
+// var split =whight.toLowerCase().split("k")
+// console.log(split)
+// var floor =Math.floor(Math.random()*4)+2
+// console .log("your wheight is " + split[0]+floor )
+
+//question #8
+
+// var input=prompt("input a secret number")
+// var rand =Math.random()*2+1
+// console.log(Math.floor(rand))
+// if (input ==Math.floor(rand)){
+//     alert ("congratulation : correct code" )
+// }else {
+//     alert( "wrong code")
+// }
+
+
+
+
+
+
+
+
